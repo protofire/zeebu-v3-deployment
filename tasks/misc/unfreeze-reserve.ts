513b5c547b1cdd2932432ea3ab371f64dc4f02d5
Mijo Grabovac <Grabovac0711@gmail.com>
2025-05-16 12:43:26 +0200

    feat(unfreeze-reserve): add task to unfreeze a reserve in the Aave V3 protocol

---
+++ tasks/misc/unfreeze-reserve.ts
@@ -0,0 +1,31 @@
+import { task } from "hardhat/config";
+import { ethers } from "hardhat";
+import dotenv from "dotenv";
+dotenv.config();
+
+// Usage:
+// npx hardhat unfreeze-reserve --network <network> --asset <asset-address>
+
+task("unfreeze-reserve", "Unfreeze a reserve in the Aave V3 protocol")
+  .addParam("asset", "The address of the asset (reserve) to unfreeze")
+  .setAction(async ({ asset }, hre) => {
+    const POOL_ADDRESSES_PROVIDER_ADDRESS = process.env.POOL_ADDRESSES_PROVIDER_ADDRESS || "<POOL_ADDRESSES_PROVIDER_ADDRESS_HERE>";
+    if (!ethers.utils.isAddress(POOL_ADDRESSES_PROVIDER_ADDRESS)) {
+      throw new Error("Invalid PoolAddressesProvider address. Set POOL_ADDRESSES_PROVIDER_ADDRESS in your .env file.");
+    }
+    if (!ethers.utils.isAddress(asset)) {
+      throw new Error("Invalid asset address provided.");
+    }
+    const [signer] = await hre.ethers.getSigners();
+    console.log(`Using signer: ${signer.address}`);
+    const poolAddressesProvider = await ethers.getContractAt("PoolAddressesProvider", POOL_ADDRESSES_PROVIDER_ADDRESS, signer);
+    const poolConfiguratorAddress = await poolAddressesProvider.getPoolConfigurator();
+    if (!ethers.utils.isAddress(poolConfiguratorAddress)) {
+      throw new Error("Invalid PoolConfigurator address fetched from PoolAddressesProvider.");
+    }
+    const poolConfigurator = await ethers.getContractAt("PoolConfigurator", poolConfiguratorAddress, signer);
+    console.log(`Unfreezing reserve for asset: ${asset}`);
+    const tx = await poolConfigurator.setReserveFreeze(asset, false);
+    await tx.wait();
+    console.log(`Reserve for asset ${asset} successfully unfrozen.`);
+  });
