b4b87fdd92e3ce9ba00aa0d31bde8edcaf998105
Mijo Grabovac <Grabovac0711@gmail.com>
2025-05-21 15:51:31 +0200

    refactor: update AaveOracle task to use hre.ethers.utils for address validation

--- tasks/misc/update-AaveOracle.ts
+++ tasks/misc/update-AaveOracle.ts
@@ -1,10 +1,8 @@
 import { task } from "hardhat/config";
-import { ethers } from "hardhat";
-import dotenv from "dotenv";
-dotenv.config();
 
 // Usage: npx hardhat update-AaveOracle --network <network> --asset <asset-address> --source <source-address>
-
+// command
+// npx hardhat update-AaveOracle --network sepolia --asset 0x6098Bc6CA2fDFDa186847878726AFBad1d01f13D --source  0x1b44F3514812d835EB1BDB0acB33d3fA3351Ee43
 const aaveOracleAddresses: { [network: string]: string } = {
   baseSepolia: "0xa4145d8c5A6Dac1882268F67dc71f514666052B2",
   sepolia: "0xB7c437D99bD1EEd8E8D0eC4fE865b70EfBFAc1f3",
@@ -15,19 +13,19 @@ task("update-AaveOracle", "Update the price source (aggregator) for an asset in 
   .addParam("asset", "The address of the asset to update the price source for")
   .addParam("source", "The new price source (aggregator) address for the asset")
   .setAction(async ({ asset, source }, hre) => {
-    if (!ethers.utils.isAddress(asset)) {
+    if (!hre.ethers.utils.isAddress(asset)) {
       throw new Error("Invalid asset address provided.");
     }
-    if (!ethers.utils.isAddress(source)) {
+    if (!hre.ethers.utils.isAddress(source)) {
       throw new Error("Invalid source address provided.");
     }
     const [signer] = await hre.ethers.getSigners();
     const network = hre.network.name;
     const AAVE_ORACLE_ADDRESS = aaveOracleAddresses[network];
-    if (!AAVE_ORACLE_ADDRESS || !ethers.utils.isAddress(AAVE_ORACLE_ADDRESS)) {
+    if (!AAVE_ORACLE_ADDRESS || !hre.ethers.utils.isAddress(AAVE_ORACLE_ADDRESS)) {
       throw new Error(`AaveOracle address for network ${network} is not set in aaveOracleAddresses map.`);
     }
-    const aaveOracle = await ethers.getContractAt("AaveOracle", AAVE_ORACLE_ADDRESS, signer);
+    const aaveOracle = await hre.ethers.getContractAt("AaveOracle", AAVE_ORACLE_ADDRESS, signer);
     console.log(`Updating price source for asset: ${asset} to new source: ${source} on network: ${network}`);
     const tx = await aaveOracle.setAssetSources([asset], [source]);
     await tx.wait();
